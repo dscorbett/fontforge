fc6dd2e00b024d67d4e467c0a75d1df9433ae975
David Corbett <corbett.dav@northeastern.edu>
2020-12-20 13:42:52 -0500

    Revert "Merge pull request #3855 from jtanx/cmake3"
    This reverts commit 2511164b3d0d0f052306b6250c6b31afe7014b8c, reversing
    changes made to 547cb707316dae9ee94a63c0ce801a46154bbcf9.

--- tests/test927.py
+++ tests/test927.py
@@ -1,12 +1,12 @@
-#Needs: fonts/CantarellMin.woff2
+#Needs: fonts/Ambrosia.sfd
 
 import fontforge
 import sys
 
 sfd = fontforge.open(sys.argv[1])
-sfd.generate("CantarellMin.woff2")
+sfd.generate("Ambrosia.woff2")
 sfd.close()
 
-woff = fontforge.open("CantarellMin.woff2")
-woff.save("CantarellMin.sfd")
+woff = fontforge.open("Ambrosia.woff2")
+woff.save("Ambrosia.sfd")
 woff.close()
